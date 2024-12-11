359fe66853b370de3dc8f27926e67532cf2ea8fb
Ryan McGuire <rmva@mac.mynetworksettings.com>
2024-12-11 14:16:00 -0500

    type update

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -3,7 +3,7 @@ import AddForm from '../ui/add/add-form';
 export default function Page() {
 	return (
 		<main className='max-w-screen-lg mx-auto'>
-			<p className='text-center'>Let's add an item.</p>
+			<p className='text-center'>Add an item.</p>
 			<AddForm />
 		</main>
 	);
